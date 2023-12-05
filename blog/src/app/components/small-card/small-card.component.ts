b9974b27343b311e76c6f17f5c033b23eb6ee449
Lucas Vasconcelos <luo_bola@hotmail.com>
2023-12-05 15:48:54 -0300

    feito os ajustes dinicamicos dos componentes e propriedades

--- blog/src/app/components/small-card/small-card.component.ts
+++ blog/src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,6 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './small-card.component.html',
   styleUrl: './small-card.component.css'
 })
-export class SmallCardComponent {
+export class SmallCardComponent implements OnInit{
+  /*
+    * criação dos parametros para chamada dinamica
+  */
+  @Input()
+  photoCover_small_card:string = ""
+  @Input()
+  cardTitle_small_card:string = ""
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }

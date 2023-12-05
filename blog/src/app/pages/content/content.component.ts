9bbbbd123b4ce63287cafe557f3c202d8f96b7d2
Lucas Vasconcelos <luo_bola@hotmail.com>
2023-12-05 16:17:47 -0300

    adicionando mais paginas

---
+++ blog/src/app/pages/content/content.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './content.component.html',
+  styleUrl: './content.component.css'
+})
+export class ContentComponent implements OnInit{
+
+  constructor(){
+
+  }
+
+
+  ngOnInit(): void {
+
+  }
+
+}
